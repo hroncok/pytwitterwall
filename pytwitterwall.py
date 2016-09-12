be4df22b2ee872fe29a8358bf5c3688c2ea21f8f
Miro Hrončok <miro@hroncok.cz>
2016-09-12 21:51:34 +0200

    Add template filters

--- pytwitterwall.py
+++ pytwitterwall.py
@@ -2,10 +2,12 @@ import base64
 import configparser
 import time
 import sys
+from datetime import datetime
 
 import click
+import jinja2
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 class TwitterWall:
@@ -13,6 +15,7 @@ class TwitterWall:
 
     def __init__(self, api_key, api_secret):
         self.session = self._create_session(api_key, api_secret)
+        self.last_seen = 0
 
     def _create_session(self, api_key, api_secret):
         session = requests.Session()
@@ -47,7 +50,8 @@ class TwitterWall:
     def tweets(self, query, count):
         """Performs an initial search, returns the tweets DESC"""
         statuses = self.search(q=query, count=count, result_type='recent')
-        self.last_seen = statuses[0]['id']
+        if statuses:
+            self.last_seen = statuses[0]['id']
         self.query = query
         return statuses
 
@@ -111,6 +115,70 @@ def credentials(path):
 
 
 app = Flask(__name__)
+
+
+@app.template_filter('time')
+def convert_time(text):
+    """Convert the time format to a different one"""
+    dt = datetime.strptime(text, '%a %b %d %H:%M:%S %z %Y')
+    return dt.strftime('%c')
+
+
+def combine(text, starts, append):
+    """Combine intermediate tweet representation to one str"""
+    strlist = []
+    i = 0
+    while True:
+        oldi = i
+        while i not in starts and i < len(text):
+            i += 1
+        strlist.append(text[oldi:i])
+        if not i < len(text):
+            break
+        strlist.append(starts[i]['html'])
+        i = starts[i]['indices'][1]
+    strlist.append(append)
+    return ''.join(strlist)
+
+
+def htmlzie(entities, text):
+    """Converts a tweet to rich HTML form"""
+    starts = {}  # starting indexes will be keys
+    append = ''  # what shell go to the end of the tweet
+    link = '<a href="{}">{}</a>'
+    twitter = 'https://twitter.com/'
+    for k, v in entities.items():
+        for entity in v:
+            s, e = entity['indices']
+            snip = text[s:e]
+            if k == 'user_mentions':
+                name = entity['screen_name']
+                html = link.format(twitter + name, snip)
+            elif k == 'hashtags':
+                hashtag = entity['text']
+                url = url_for('wall', hashtag=hashtag.lower())
+                html = link.format(url, snip)
+            elif k == 'symbols':
+                symbol = entity['text']
+                url = twitter + 'search?q=%24{}&src=ctag'.format(symbol)
+                html = link.format(url, snip)
+            elif k in ['media', 'extended_entities', 'urls']:
+                html = link.format(entity['url'], entity['display_url'])
+                if k != 'urls':
+                    append = '<br /><img src="{}:thumb" />'.format(
+                        entity['media_url_https'])
+            else:
+                assert 'Not gonna happen'
+            entity['html'] = html
+            starts[s] = entity
+    return combine(text, starts, append)
+
+
+@app.template_filter('enrich')
+def enrich_tweet(tweet):
+    """Add links and media tot he tweet"""
+    text = htmlzie(tweet['entities'], tweet['text'])
+    return jinja2.Markup(text)
 
 
 @app.route('/')
